e3daa3632eefcdf92af832c5ece0c6633e748529
Kristina <ccthw@protonmail.com>
2020-03-07 18:21:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -57,32 +57,57 @@ function appendPageLinks () {
    }
 } 
 
-// create and append the elements of searchbar
-const div = document.querySelector('div.page-header'); 
-const button = document.createElement('button'); 
-button.className = 'student-search'; 
-button.textContent = 'Search'; 
-div.appendChild(button); 
-const input = document.createElement('input'); 
-input.className = 'student-search';
-div.appendChild(input);  
-const searchedName = input.value.toLowerCase();
+// Call functions for showing a page and list pagination 
 
-// make searchbar funtional
-function searchBar () {
-   for (let i = 0; i < list.length; i ++) {
-      const listDiv = list.firstElementChild; 
-      const span = listDiv.lastElementChild; 
-      const h3 = span.previousElementSibling; 
-      const studentName = h3.textContent || h3.innerHTML;  
-      if (studentName.indexOf(searchedName) > -1) {
-         list.style.display = ''; 
-      } else {
-         list.style.display = 'none'; 
-      }
+showPage (list, 1); 
+appendPageLinks (list); 
+
+// Create and append the elements of searchbar
+
+const pageheaderDiv = document.querySelector('div.page-header'); 
+const searchbarDiv = document.createElement('div'); 
+pageheaderDiv.appendChild(searchbarDiv); 
+searchbarDiv.className = 'student-search'; 
+const searchbarInput = document.createElement('input'); // create input
+searchbarInput.type = 'text'; 
+searchbarInput.setAttribute('id', 'input'); 
+searchbarInput.placeholder = 'search...'; 
+searchbarDiv.appendChild(searchbarInput);  
+const searchbarButton = document.createElement('button'); // create button 
+searchbarButton.textContent = 'Search'; 
+searchbarDiv.appendChild(searchbarButton); 
+const searchedName = searchbarInput.value.toLowerCase(); // input value
+
+// A function for searching 
+
+function searchbar (searchedName) {
+   let searchbarList = []; // an empty array to get filtered list items in
+   for (let i = 0; i < list.length; i ++) { // looping through students' names 
+     const studentNameElement = list[i].getElementsByTagName('h3'); 
+     const studentName = studentNameElement.innerHTML;
+     if (searchedName.indexOf(studentName)) { // check if the name is equal to the searched one
+       searchbarList.push(list[i]); 
+     } 
+   }
+   if (searchbarList.length === 0) { // message if the name is not found
+   let message = document.createElement('p'); 
+   pageheaderDiv.appendChild(message); 
+   message.innerHTML = `No matches are found by the search.`;
+   } else {
+      showPage(searchbarList, 1); // if the name is found, show it
+      appendPageLinks(searchbarList);
    }
 }
 
-showPage(list, 1); 
-appendPageLinks(list); 
-searchBar();
+// A handler for the input 
+
+searchbarInput.addEventListener('keyup', () => {
+   searchbar (searchedName);
+});
+
+// A handler for the button 
+
+searchbarButton.addEventListener('click', () => {
+   searchbar (searchedName);
+});
+
